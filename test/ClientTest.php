526813e2ceb1283fb82243c566563b327116333d
Matt Bonneau <matt@bonneau.net>
2017-09-12 15:37:45 -0400

    Update README.md and add a test

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -3,6 +3,9 @@
 namespace Rx\Websocket\Test;
 
 use React\EventLoop\Factory;
+use Rx\Websocket\Client;
+use Rx\Websocket\MessageSubject;
+use Rx\Websocket\Server;
 
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
@@ -10,6 +13,7 @@ class ClientTest extends \PHPUnit_Framework_TestCase
     {
         $loop = Factory::create();
 
+        // expecting connection error
         $client = new \Rx\Websocket\Client('ws://127.0.0.1:12340/', false, [], $loop);
 
         $errored = false;
@@ -24,5 +28,37 @@ class ClientTest extends \PHPUnit_Framework_TestCase
         $loop->run();
 
         $this->assertTrue($errored);
+    }
+
+    public function testRequestEndOnDispose()
+    {
+        $this->markTestSkipped();
+        $loop = Factory::create();
+
+        $server = new Server('tcp://127.0.0.1:1234', false, [], $loop);
+        $serverDisp = $server->subscribe(function (MessageSubject $ms) {
+            $ms->map('strrev')->subscribe($ms);
+        });
+
+        $value = null;
+
+        $client = new Client('ws://127.0.0.1:1234/', false, [], $loop);
+        $client
+            ->subscribe(function (MessageSubject $ms) use ($serverDisp) {
+                $ms->onNext('Hello');
+                $ms
+                    ->finally(function () use ($serverDisp) {
+                        $serverDisp->dispose();
+                    })
+                    ->take(1)
+                    ->subscribe(function ($x) use (&$value) {
+                        $this->assertNull($value);
+                        $value = $x;
+                    });
+            });
+
+        $loop->run();
+
+        $this->assertEquals('olleH', $value);
     }
 }
