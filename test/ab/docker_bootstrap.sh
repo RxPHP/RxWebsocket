1f05d949c85cbf2d15c28d3f9d10db0f50f7686a
Matt Bonneau <matt@bonneau.net>
2021-09-25 13:47:18 -0400

    Add IP address to hosts file in docker test runner

--- test/ab/docker_bootstrap.sh
+++ test/ab/docker_bootstrap.sh
@@ -3,9 +3,9 @@ set -x
 
 echo "Running $0"
 
-echo Adding "$1 host.ratchet.internal" to /etc/hosts file
+echo Adding "$1 host.dock.internal" to /etc/hosts file
 
-echo $1 host.ratchet.internal >> /etc/hosts
+echo $1 host.dock.internal >> /etc/hosts
 
 echo /etc/hosts contains:
 cat /etc/hosts
--- test/ab/run_ab_tests.sh
+++ test/ab/run_ab_tests.sh
@@ -19,12 +19,19 @@ php -d memory_limit=256M testServer.php &
 SERVER_PID=$!
 sleep 3
 
+if [ "$RUNNER_OS" = "Linux" ]; then
+  IPADDR=`hostname -I | cut -f 1 -d ' '`
+else
+  IPADDR=`ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | head -1 | tr -d 'adr:'`
+fi
+
 docker run --rm \
        \
       -v ${PWD}:/config \
       -v ${PWD}/reports:/reports \
       --name rxwsfuzzingclient \
-      crossbario/autobahn-testsuite wstest -m fuzzingclient -s /config/fuzzingclient.json
+      crossbario/autobahn-testsuite /bin/sh -c "sh /config/docker_bootstrap.sh $IPADDR; wstest -m fuzzingclient -s /config/fuzzingclient.json"
+
 sleep 12
 
 kill $SERVER_PID
